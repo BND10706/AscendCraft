84f63edcdf57989cd841f72044941bb09c3d553b
Brandon.DeVore <Brandon.DeVore@publix.com>
2024-09-04 13:31:10 -0400

    Main Added

---
+++ src/main/java/com/ascendcraft/ascendcraft/AscendCraft.java
@@ -0,0 +1,10 @@
+package com.ascendcraft.ascendcraft;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod("ascendcraft")
+public class AscendCraft {
+    public AscendCraft() {
+        // Initialize your mod here
+    }
+}
